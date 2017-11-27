5355de2d36371b565536e4910b1f20e946ea47f8
Vincent B <evilznet@gmail.com>
2017-11-27 20:00:13 +0100

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace small_docker_test
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
